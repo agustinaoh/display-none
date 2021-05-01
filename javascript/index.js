f26c2df9bf5a152d8d2d3e827c124124cae5c799
agustina ohan <agustina.oh@gmail.com>
2021-05-02 00:31:35 +0200

    menu open/close

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,26 @@
 console.log('hi');
+
+
+// selectors
+
+const hamburgerMenu = document.querySelector('#hamburger');
+const closeIcon = document.querySelector('#close')
+const menu = document.querySelector('.menu');
+
+
+// functions
+
+function openMenu(){
+  hamburgerMenu.classList.add('hidden');
+  menu.classList.remove('hidden');
+};
+
+function closeMenu(){
+  hamburgerMenu.classList.remove('hidden');
+  menu.classList.add('hidden');
+}
+
+// event listeners
+
+hamburgerMenu.addEventListener('click', openMenu);
+closeIcon.addEventListener('click', closeMenu);
