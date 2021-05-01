e5950a5d00fa49393f6e0b3db1843f7801573e5d
agustina ohan <agustina.oh@gmail.com>
2021-05-02 01:03:43 +0200

    menu css

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,15 @@
 console.log('hi');
 
 
+
 // selectors
 
 const hamburgerMenu = document.querySelector('#hamburger');
 const closeIcon = document.querySelector('#close')
 const menu = document.querySelector('.menu');
 
+const carousel = document.querySelector('.carousel')
+const newsSection = document.querySelector('#news')
 
 // functions
 
@@ -19,6 +22,7 @@ function closeMenu(){
   hamburgerMenu.classList.remove('hidden');
   menu.classList.add('hidden');
 }
+
 
 // event listeners
 
