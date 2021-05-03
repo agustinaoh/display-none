3902742023eae489d308243c072e3dd9bf5358c4
agustina ohan <agustina.oh@gmail.com>
2021-05-03 17:10:39 +0200

    gsap set

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,8 @@
 console.log('hi');
 
-
+gsap.registerPlugin(ScrollTrigger);
 
 // selectors
-
 const hamburgerMenu = document.querySelector('#hamburger');
 const closeIcon = document.querySelector('#close')
 const menu = document.querySelector('.menu');
@@ -12,7 +11,6 @@ const carousel = document.querySelector('.carousel')
 const newsSection = document.querySelector('#news')
 
 // functions
-
 function openMenu(){
   hamburgerMenu.classList.add('hidden');
   menu.classList.remove('hidden');
@@ -23,8 +21,43 @@ function closeMenu(){
   menu.classList.add('hidden');
 }
 
+// GSAP
+function initIntro() {
+  // set up scrollTrigger animation for the when the intro scrolls out
+  let designArea = gsap.timeline({
+      scrollTrigger: {
+          trigger: '#design-image', //referencia de scroll
+          scrub: 1,
+          start: "top bottom", // position of trigger meets the scroller position
+          end: "800 center" // div, view
+      }
+  });
+
+  designArea.from('.intro__title', {x: -400, duration: 3, opacity: 0, ease: 'power4'})
+  designArea.from('#find-your-style', {x: 500, duration: 3}, '-= 3' )
+
+  gsap.from('.universe-t', {y: 400, duration: 1, opacity: 0})
+
+  let telas = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.telas',
+      scrub: 1,
+      start: "top bottom",
+      end: "center top",
+      markers: true
+    }
+  });
+
+  telas.from('.fc-1', {y: 200, duration: 1, opacity: 0})
+        .from('.fc-2', {y: 200, duration: 2, opacity: 0}, '-= 1' )
+        .from('.fc-3', {y: 200, duration: 3, opacity: 0}, '-= 2' )
+        .from('.fc-4', {y: 200, duration: 3, opacity: 0}, '-= 3' )
+}
 
 // event listeners
-
 hamburgerMenu.addEventListener('click', openMenu);
 closeIcon.addEventListener('click', closeMenu);
+
+window.onload = () => {
+  initIntro()
+};
